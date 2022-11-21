dac9d75561ba556f3c21d247f9f97abfd953d188
Kimbo0o <kimkoch1000@gmail.com>
2022-11-21 20:11:13 +0100

    updated shadows

--- sources/Experience/World.js
+++ sources/Experience/World.js
@@ -17,14 +17,34 @@ export default class World {
 
   setRoom() {
     this.room = {};
-    this.room.model = this.resources.items.roomModel.scene;
+    const scene = this.resources.items.roomModel.scene;
+    scene.traverse((node) => {
+      if (node.isMesh) {
+        console.log("node", node);
+        node.castShadow = true;
+        node.receiveShadow = true;
+      }
+    });
+    this.room.model = scene;
+
+    this.room.model.tra;
     console.log("scene", this.room.model);
 
     this.scene.add(this.room.model);
 
-    const directionalLight = new THREE.DirectionalLight("#ffffff", 3);
+    const directionalLight = new THREE.DirectionalLight("#ffffff", 5);
     directionalLight.position.set(5, 5, 5);
+    directionalLight.castShadow = true;
     this.scene.add(directionalLight);
+
+    const testLight = new THREE.PointLight("#0000ff", 10);
+    // testLight.castShadow = true;
+    testLight.position.set(-1.4, 1.51163, 0.5);
+    this.scene.add(testLight);
+
+    const sphereSize = 0.1;
+    const pointLightHelper = new THREE.PointLightHelper(testLight, sphereSize);
+    scene.add(pointLightHelper);
   }
 
   resize() {}
