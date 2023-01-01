b3475e42f296f9b52c3fed87f03ab643639debe1
Kimbo0o <kimkoch1000@gmail.com>
2023-01-01 22:24:54 +0100

    added second camera

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,7 +7,7 @@ import {
   Environment,
   PerspectiveCamera,
 } from "@react-three/drei";
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 import FloorLampLight from "./FloorLampLight";
 import Website from "./Website";
 import BackgroundLight from "./BackgroundLight";
@@ -18,6 +18,7 @@ import PhotoCamera from "./PhotoCamera";
 import PrinterPlate from "./PrinterPlate";
 import Name from "./Name";
 import AdditionalLight from "./AdditionalLight";
+import { useControls } from "leva";
 
 const Experience = (props: { lightMode: boolean }) => {
   const { scene } = useGLTF("/roomModel.glb");
@@ -31,16 +32,54 @@ const Experience = (props: { lightMode: boolean }) => {
     });
   });
 
+  const [currentCamera, setCurrentCamera] = useState<"orbit" | "screen">(
+    "orbit"
+  );
+
+  const goToScreen = () => {
+    setCurrentCamera("screen");
+  };
+
+  const goToOrbit = () => {
+    setCurrentCamera("orbit");
+  };
+
+  const { x, y, z } = useControls("camera", {
+    x: { value: 0.5, min: 0, max: 10, step: 0.01 },
+    y: { value: 1.51, min: 0, max: 10, step: 0.01 },
+    z: { value: -0.135, min: -5, max: 10, step: 0.01 },
+  });
+
   return (
     <Canvas shadows>
-      <PerspectiveCamera
-        fov={25}
-        near={0.1}
-        far={2000}
-        position={[10, 6, 10]}
-        makeDefault
-      />
-      <OrbitControls makeDefault target={[0, 1.5, 0]} />
+      {currentCamera === "orbit" && (
+        <group>
+          <PerspectiveCamera
+            fov={25}
+            near={0.1}
+            far={2000}
+            position={[10, 6, 10]}
+            makeDefault
+          />
+          <OrbitControls makeDefault target={[0, 1.5, 0]} />
+        </group>
+      )}
+      {currentCamera === "screen" && (
+        <group>
+          <PerspectiveCamera
+            fov={25}
+            near={0.1}
+            far={2000}
+            position={[x, y, z]}
+            makeDefault
+          />
+          <OrbitControls
+            makeDefault
+            target={[-1.3, 1.51, -0.135]}
+            enableZoom={false}
+          />
+        </group>
+      )}
 
       {props.lightMode && <Environment files="/potsdamer_platz_1k.hdr" />}
       {!props.lightMode && <Environment files="/dikhololo_night_1k.hdr" />}
@@ -60,8 +99,11 @@ const Experience = (props: { lightMode: boolean }) => {
       />
 
       <primitive castShadow receiveShadow object={scene} />
-
-      <Website />
+      <Website
+        isActive={currentCamera === "screen"}
+        onGoToScreen={goToScreen}
+        onGoBack={goToOrbit}
+      />
       <SeatTop />
       <VrGlasses />
       <PhotoCamera />
--- src/components/Website.tsx
+++ src/components/Website.tsx
@@ -1,29 +1,75 @@
 import { Html } from "@react-three/drei";
 import { useControls } from "leva";
+import {
+  InformationCircleIcon,
+  ArrowUturnLeftIcon,
+} from "@heroicons/react/20/solid";
 
-const Website = () => {
+const Website = (props: {
+  isActive: boolean;
+  onGoToScreen: () => void;
+  onGoBack: () => void;
+}) => {
   const { x, y, z, distanceFactor } = useControls("html", {
     x: { value: -1.3, min: -5, max: 5, step: 0.01 },
     y: { value: 1.51, min: -5, max: 5, step: 0.01 },
     z: { value: -0.135, min: -5, max: 5, step: 0.01 },
-    distanceFactor: { value: 0.128, min: 0, max: 1, step: 0.001 },
+    distanceFactor: { value: 0.15, min: 0, max: 1, step: 0.001 },
   });
 
   return (
-    <Html
-      transform
-      distanceFactor={distanceFactor}
-      position={[x, y, z]}
-      rotation-y={Math.PI / 2}
-      occlude
-    >
-      <iframe
-        src="https://kimdanielkoch.de"
-        width={1920}
-        height={1080}
-        className="border-none w-[1920px] h-[1080px] rounded-lg bg-black"
-      />
-    </Html>
+    <group>
+      {/* infopoint button */}
+      {!props.isActive && (
+        <Html
+          transform
+          position={[x, y + 0.3, z]}
+          rotation-y={Math.PI / 2}
+          occlude
+          distanceFactor={5}
+        >
+          <button
+            className="rounded-full p-0.5 bg-white/20 hover:bg-white/50 animate-pulse"
+            onClick={props.onGoToScreen}
+          >
+            <InformationCircleIcon className="w-2 h-2 text-white" />
+          </button>
+        </Html>
+      )}
+      {/* back button */}
+      {props.isActive && (
+        <Html
+          transform
+          position={[x, y + 0.3, z]}
+          rotation-y={Math.PI / 2}
+          occlude
+          distanceFactor={3}
+        >
+          <button
+            className="rounded-full p-0.5 bg-white/20 hover:bg-white/50 animate-pulse"
+            onClick={props.onGoBack}
+          >
+            <ArrowUturnLeftIcon className="w-2 h-2 text-white " />
+          </button>
+        </Html>
+      )}
+
+      {/* iframe */}
+      <Html
+        transform
+        distanceFactor={distanceFactor}
+        position={[x, y, z]}
+        rotation-y={Math.PI / 2}
+        occlude
+      >
+        <iframe
+          src="https://kimdanielkoch.de"
+          width={1600}
+          height={900}
+          className="border-none w-[1600px] h-[900px] rounded-lg bg-black"
+        />
+      </Html>
+    </group>
   );
 };
 
