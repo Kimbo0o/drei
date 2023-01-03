96da91cf38c4c368f943bdc4052ab894f5afc9c5
Kimbo0o <kimkoch1000@gmail.com>
2023-01-03 23:14:40 +0100

    added loader

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import "./App.css";
 
 import Experience from "./components/Experience";
 import Info from "./components/Info";
+import { Suspense } from "react";
+import Spinner from "./components/Spinner";
 
 function App() {
   const isRecommendedBrowser =
@@ -17,7 +19,15 @@ function App() {
   return (
     <div className="w-screen h-screen relative">
       <Leva hidden={!window.location.search.includes("debug")} />
-      <Experience />
+      <Suspense
+        fallback={
+          <div className="flex w-full h-full items-center justify-center bg-gray-900">
+            <Spinner />
+          </div>
+        }
+      >
+        <Experience />
+      </Suspense>
       <Info />
       {!isRecommendedBrowser && (
         <div className="absolute top-4 left-0 w-full text-center text-gray-300">
