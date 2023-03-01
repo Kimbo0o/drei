dbedda13b684069a720104628594da95a2316509
Kimbo0o <kimkoch1000@gmail.com>
2023-03-01 18:17:27 +0100

    Improved printer plate position calculation

--- src/components/PrinterPlate.tsx
+++ src/components/PrinterPlate.tsx
@@ -1,28 +1,13 @@
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { useState } from "react";
 
 const SeatTop = () => {
   const model = useGLTF("/printerPlate.glb");
 
-  const [positionX, setPositionX] = useState(-1.01);
-  const [up, setUp] = useState(true);
-
-  useFrame(() => {
+  useFrame(({ clock }) => {
     if (model?.scene?.children.length) {
-      if (positionX <= -1.01) {
-        setUp(true);
-      } else if (positionX >= -0.9) {
-        setUp(false);
-      }
-
-      if (up) {
-        setPositionX(positionX + 0.002);
-      } else {
-        setPositionX(positionX - 0.002);
-      }
-
-      model.scene.children[0].position.x = positionX;
+      model.scene.children[0].position.x =
+        Math.sin(clock.getElapsedTime() * 3) * 0.05 - 0.95;
     }
   });
 
